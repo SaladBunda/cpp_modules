992e71a00e6ceea2b29370cbde9dd632e70e6565
Ahmed El Maazouzi <ael-maaz@c1r3p4.1337.ma>
2024-09-13 15:53:15 +0100

    just added ex06 for cpp1

---
+++ cpp1/ex06/Harl.cpp
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ael-maaz <ael-maaz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/10 12:01:42 by codespace         #+#    #+#             */
+/*   Updated: 2024/09/13 15:47:31 by ael-maaz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+void Harl::debug(void)
+{
+    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+}
+
+void Harl::info(void)
+{
+    std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+}
+
+void Harl::warning(void)
+{
+    std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
+}
+
+void Harl::error(void)
+{
+    std::cout <<  "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+void Harl::other(void)
+{
+    std::cout <<  "* complaining about random stuff *" << std::endl;
+}
+
+void Harl::complain(std::string level)
+{
+    Harl obj;
+    void (Harl::*pointer[])()={&Harl::debug, &Harl::info,&Harl::warning,&Harl::error, &Harl::other};
+    std::string option[] = {"DEBUG","INFO", "WARNING", "ERROR"};
+    int i = 0;
+    while(level != option[i] && i<4)
+        i++;
+    switch (i)
+    {
+        case 0:
+            (obj.*pointer[i++])();
+        case 1:
+            (obj.*pointer[i++])();
+        case 2:
+            (obj.*pointer[i++])();
+        case 3:
+            (obj.*pointer[i++])();
+            break ;
+        default:
+            (obj.*pointer[i])();
+    }
+            
+            
+}
---
+++ cpp1/ex06/Harl.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ael-maaz <ael-maaz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/09 23:13:26 by codespace         #+#    #+#             */
+/*   Updated: 2024/09/13 12:37:33 by ael-maaz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+# define HARL_HPP
+
+#include <iostream>
+#include <string>
+
+class Harl
+{
+    private:
+        void debug(void);
+        void info(void);
+        void warning(void);
+        void error(void);
+        void other(void);
+    public:
+        void complain(std::string level);
+};
+
+#endif
