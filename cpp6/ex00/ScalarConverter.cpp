eade7946925e54046bb0e9018e3d5a5528474e32
Ahmed El Maazouzi <ael-maaz@c1r7p5.1337.ma>
2025-01-21 12:47:32 +0100

    aaaaa

--- cpp6/ex00/ScalarConverter.cpp
+++ cpp6/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: ael-maaz <ael-maaz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 09:14:03 by ael-maaz          #+#    #+#             */
-/*   Updated: 2025/01/20 14:47:40 by ael-maaz         ###   ########.fr       */
+/*   Updated: 2025/01/21 11:00:39 by ael-maaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,21 +71,10 @@ int	ft_atoi(char *s, int *error)
 	{
 		num = num * 10 +(s[i++] - 48);
 	}
-	if (s[i] != '\0' || num == 0 || (num * sign) > 2147483647 || (num * sign) < -2147483648)
+	if ((s[i] != '\0' && num == 0) || s[i] != '\0' || (num * sign) > 2147483647 || (num * sign) < -2147483648)
 		*error = 1;
 	return (num * sign);
 }
-
-
-// double testZero(std::string str)
-// {
-// 	int i = 0;
-// 	while(str[i] && str[i] == '0')
-// 		i++;
-// 	if(str[i] == '.')
-// 		i++;
-	
-// }
 
 int pretest(std::string var)
 {
@@ -107,13 +96,39 @@ int pretest(std::string var)
 		return 1;
 }
 
+int test_side(std::string var)
+{
+	int end;
+	int start = var.find('.') + 1;
+	if(var.back() == 'f')
+		end = var.length()- var.find('.') - 2;
+	else
+		end = var.length()- var.find('.') - 1;
+		
+	
+	std::string first = var.substr(0,var.find('.'));
+	std::string seconds = var.substr(start,end);
+
+	int error = 0;
+	ft_atoi((char*) first.c_str(), &error);
+	ft_atoi((char*) seconds.c_str(), &error);
+	if(error == 1)
+	{
+		std::cout << "ko" << std::endl;
+		return 1;
+	}
+	std::cout << "ok" << std::endl;
+	return 0;
+}
+
 
 void ScalarConverter::convert(std::string var)
 {
 	if(pretest(var) == 0)
 		return ;
-	if(var.find('.') != var.npos)
+	if(var.find('.') != var.npos)//if there is a dot present in the string
 	{
+		test_side(var);
 		std::string afterpoint = var.substr(var.find('.') + 1);
 		std::cout <<afterpoint << std::endl;
 		if(afterpoint.find('.') != afterpoint.npos || afterpoint == "")
@@ -121,8 +136,6 @@ void ScalarConverter::convert(std::string var)
 			std::cout << "invalid number" << std::endl;
 			return ;
 		}
-		
-		std::cout << "goes to float tester" << std::endl;
 		double number = atof((char *)var.c_str());
 		if(afterpoint.back() == 'f')
 		{
@@ -170,7 +183,7 @@ void ScalarConverter::convert(std::string var)
 			float numf = static_cast<float>(num);
 			double numd = static_cast<double>(num);
 			std::cout << std::fixed << std::setprecision(1);
-			std::cout << "float: " << numf<< "f" << std::endl;
+			std::cout << "float: " << numf << "f" << std::endl;
 			std::cout << "double: " << numd << std::endl;
 			
 		}
