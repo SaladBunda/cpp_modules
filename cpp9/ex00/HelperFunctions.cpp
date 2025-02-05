b75eb437bc9cd400e2d5d99b810f1651588cb507
Ahmed El Maazouzi <ael-maaz@c2r6p1.1337.ma>
2025-02-05 13:57:02 +0100

    workign on ex00

--- cpp9/ex00/HelperFunctions.cpp
+++ cpp9/ex00/HelperFunctions.cpp
@@ -6,29 +6,48 @@
 /*   By: ael-maaz <ael-maaz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 20:34:27 by ael-maaz          #+#    #+#             */
-/*   Updated: 2025/02/04 20:52:36 by ael-maaz         ###   ########.fr       */
+/*   Updated: 2025/02/05 13:56:54 by ael-maaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 
+int test_date_string(std::string date)
+{
+	for(int i = 0;i < date.size();i++)
+	{
+		if(date[i] != '-' || (date[i]  < '0' || date[i] >'9'))
+			return 1;
+	}
+	return 0;
+}
+
 int parse_date(std::string date)
 {
 	std::string arr[3];
 	int j = 0;
+	int k = 0;
 	for(int i =0; i < date.size();i++)
 	{
 		if(date[i] == '-')
 		{
-			arr[j] = date.substr(i,j - i + 1);
+			arr[j] = date.substr(k,i - k);
 			j++;
 			i++;
+			k = i;
+		}
+		else if(i == date.size() - 1)
+		{
+			arr[j] = date.substr(k,i - k + 1);
+			j++;
+			i++;
+			k = i;
 		}
 		if(j > 3)
 			break;
 	}
 	for(int i = 0;i < 3;i++)
-		std::cout << arr[i] <<std::endl;
+		std::cout << i << " " <<arr[i] <<std::endl;
 	return 1;
 }
 
--- cpp9/ex00/main.cpp
+++ cpp9/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ael-maaz <ael-maaz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 20:19:19 by ael-maaz          #+#    #+#             */
-/*   Updated: 2025/02/04 20:53:33 by ael-maaz         ###   ########.fr       */
+/*   Updated: 2025/02/05 13:49:57 by ael-maaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int main(int ac, char **av)
 	{
 		// BitcoinExchange main;
 		// main.fill_data();
-		parse_date("2004-9-9");
+		parse_date("2004-9-90");
 		return 0;
 	}	
 	else
