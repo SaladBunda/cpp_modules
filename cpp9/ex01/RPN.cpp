406e7e9cf2a93b565cfee7a49dd25f77f88466a3
Ahmed El Maazouzi <ael-maaz@c1r7p1.1337.ma>
2025-02-08 12:30:14 +0100

    started ex02

--- cpp9/ex01/RPN.cpp
+++ cpp9/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: ael-maaz <ael-maaz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/07 17:17:40 by ael-maaz          #+#    #+#             */
-/*   Updated: 2025/02/07 20:37:39 by ael-maaz         ###   ########.fr       */
+/*   Updated: 2025/02/08 11:40:58 by ael-maaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,16 +44,13 @@ int RPN::ParseArg(std::string arg)
 {
 	if(TestArgChar(arg) == 1)
 		return 1;
-
 	std::stringstream s(arg);
 	std::string word;
-	
-	// int count = 0;
+	std::string operand2;
+	std::string operand1;
     while (s >> word)
 	{
-        // count++;
 		std::cout << word << std::endl;
-		// std::cout << word<< std::endl;
 		if(word.size() > 1)
 		{
 			std::cout << "Invalid number" << std::endl;
@@ -62,59 +59,37 @@ int RPN::ParseArg(std::string arg)
 		this->stk.push(word);
 		if(word.find_first_of("+-/*") != arg.npos)
 		{
-			// std::cout << "Top: " << this->stk.top() << std::endl;
 			std::string op = this->stk.top();
 			
 			if (!this->stk.empty()) 
-       			this->stk.pop();  // Safe to pop
+       			this->stk.pop();  
     		else 
         		return 1;
-			
-			// std::cout << "Top: " << this->stk.top() << std::endl;
-			std::string operand2;
 			if (!this->stk.empty()) 
+			{
 				operand2 = this->stk.top();
+       			this->stk.pop();  
+			}
 			else 
         		return 1;
-
-			
-			if (!this->stk.empty()) 
-       			this->stk.pop();  // Safe to pop
-    		else 
-        		return 1;
-
-				
-			// std::cout << "Top: " << this->stk.top() << std::endl;
-			std::string operand1;
-
-			
-			if (!this->stk.empty()) 
+			if (!this->stk.empty())
+			{
 				operand1 = this->stk.top();
+       			this->stk.pop();  
+			}
 			else 
         		return 1;
-
-				
-			if (!this->stk.empty()) 
-       			this->stk.pop();  // Safe to pop
-    		else 
-        		return 1;
-
-				
 			this->stk.push(std::to_string(PerformOperation(operand1,operand2,op)));
 		}
 	}
 	if(this->stk.size() != 1)
-	{
-		
+	{ 
 		std::cout << "Failed stack size: " << this->stk.size()<< std::endl;
 		return 1;
 	}
 	else
 		std::cout << "Result: " << this->stk.top() << std::endl;
 	
-	// for(int i = 0; i < arg.size();i++)
-	// {
-		
-	// }
+	
 	return 0;
 }
