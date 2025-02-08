86ddb361ade6206898406aff8bc13bb2e53e5651
Ahmed El Maazouzi <ael-maaz@c1r7p1.1337.ma>
2025-02-08 17:21:30 +0100

    idk why

--- cpp9/ex01/main.cpp
+++ cpp9/ex01/main.cpp
@@ -6,18 +6,28 @@
 /*   By: ael-maaz <ael-maaz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/07 17:13:38 by ael-maaz          #+#    #+#             */
-/*   Updated: 2025/02/07 20:23:29 by ael-maaz         ###   ########.fr       */
+/*   Updated: 2025/02/08 17:11:55 by ael-maaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "RPN.hpp"
 
+void ll(void)
+{
+	system("leaks RPN");
+
+}
+
 
 int main(int ac, char **av)
 {
+	atexit(ll);
 	if(ac == 2)
 	{
+		int * ntr = new int;
 		RPN main;
+		(void) ntr;
+		delete ntr;
 		std::string arg = av[1];
 		if(main.ParseArg(arg) == 1)
 			std::cout << "Error\n";
